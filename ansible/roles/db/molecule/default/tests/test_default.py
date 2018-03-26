e56de50aa8bdb43b5c0b1add3e56d94ee4a5e47b
Alexander Vorobyev <alexander.v000@gmail.com>
2018-03-27 01:05:24 +0300

    hw13: test mongo port

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -16,11 +16,13 @@ def test_hosts_file(host):
 # check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
     mongo = host.service("mongod")
+    sock = host.socket("tcp://0.0.0.0:27017")
     assert mongo.is_running
     assert mongo.is_enabled
+    assert sock.is_listening
 
 # check if configuration file contains the required line
-def test_config_file(File):
-    config_file = File('/etc/mongod.conf')
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
